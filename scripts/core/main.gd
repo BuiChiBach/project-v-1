4ce1484314b6e2c5c02ae212269ed73f24e6d4fb
Bach Bui <buichibach97@gmail.com>
2026-02-25 21:51:24 +0700

    Initial commit

---
+++ scripts/core/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world/World.tscn")
+
+
+func _on_button_quit_pressed() -> void:
+	get_tree().quit()
