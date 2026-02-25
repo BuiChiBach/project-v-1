10eac8dbd3003507a7fac0cdf400012bf3da3cb2
Bach Bui <buichibach97@gmail.com>
2026-02-26 01:20:46 +0700

    add animation movement

--- scripts/hunters/hunter_visual.gd
+++ scripts/hunters/hunter_visual.gd
@@ -1,11 +1,49 @@
 extends Node2D
 
+# Character type enumeration
+enum CharacterType { MALE, FEMALE, SKELETON, ZOMBIE }
 
-# Called when the node enters the scene tree for the first time.
+# Texture paths for different character types
+var texture_paths = {
+	CharacterType.MALE: {
+		"body": "res://assets/sprites/hunters/male_body_base.png",
+		"head": "res://assets/sprites/hunters/male_head_base.png"
+	},
+	CharacterType.FEMALE: {
+		"body": "res://assets/sprites/hunters/female_body_base.png",
+		"head": "res://assets/sprites/hunters/female_head_base.png"
+	},
+	CharacterType.SKELETON: {
+		"body": "res://assets/sprites/hunters/skeleton_body_base.png",
+		"head": "res://assets/sprites/hunters/skeleton_head_base.png"
+	},
+	CharacterType.ZOMBIE: {
+		"body": "res://assets/sprites/hunters/zombie_body_base.png",
+		"head": "res://assets/sprites/hunters/zombie_head_base.png"
+	}
+}
+
+@onready var body_sprite = $Body
+@onready var head_sprite = $Head
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+# Set character appearance by type
+func set_character_type(char_type: CharacterType) -> void:
+	if char_type in texture_paths:
+		var textures = texture_paths[char_type]
+		body_sprite.texture = load(textures["body"])
+		head_sprite.texture = load(textures["head"])
+
+
+# Alternative: Set custom textures directly
+func set_character_textures(body_texture_path: String, head_texture_path: String) -> void:
+	body_sprite.texture = load(body_texture_path)
+	head_sprite.texture = load(head_texture_path)
--- scripts/hunters/hunters.gd
+++ scripts/hunters/hunters.gd
@@ -4,22 +4,64 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var visual = $Visual
+@onready var animation_player = $Visual/AnimationPlayer
+
+var last_direction = Vector2.DOWN
+
+
+func _ready() -> void:
+	# Default to male, can be changed via set_character_type()
+	pass
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	
+	# Handle horizontal movement
+	if direction_x:
+		velocity.x = direction_x * SPEED
+		
+		# Play walk animation based on direction
+		if direction_x > 0:
+			animation_player.play("walk_right")
+			last_direction = Vector2.RIGHT
+		else:
+			animation_player.play("walk_left")
+			last_direction = Vector2.LEFT
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Handle vertical movement
+	if direction_y:
+		velocity.y = direction_y * SPEED
+		
+		# Play walk animation based on direction
+		if direction_y > 0:
+			animation_player.play("walk_down")
+			last_direction = Vector2.DOWN
+		else:
+			animation_player.play("walk_up")
+			last_direction = Vector2.UP
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	# Play idle animation based on last direction (when not moving)
+	if direction_x == 0 and direction_y == 0:
+		if last_direction == Vector2.RIGHT:
+			animation_player.play("idle_right")
+		elif last_direction == Vector2.LEFT:
+			animation_player.play("idle_left")
+		elif last_direction == Vector2.UP:
+			animation_player.play("idle_up")
+		else:
+			animation_player.play("idle_down")
 
 	move_and_slide()
+
+
+# Set the character type (MALE, FEMALE, or ENEMY)
+func set_character_type(char_type: int) -> void:
+	visual.set_character_type(char_type)
